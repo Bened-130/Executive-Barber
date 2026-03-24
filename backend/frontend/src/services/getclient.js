92386a6b7270edbf50e4c3bc1b3cf4ea903d24ef
Bened-130 <124674597+Bened-130@users.noreply.github.com>
2026-03-24 18:52:13 +0300

    Getting client into the website

---
+++ backend/frontend/src/services/getclient.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+// const CLIENTS_URL = 'http://127.0.0.1:5500/api/v1/clients'
+export async function fetchClients(){
+    try {
+          const response  = await axios.get(`${BASE_URL}/api/v1/clients`)
+          console.log(response.data.data.clients)
+          return response.data.data.clients
+    }
+    catch(err){
+        console.log('There was an error fetching data',err)
+    }
+}
